fb34ddd6b96a220af4f9d3ed3deed436ef5265f7
Alex Issa <alex.issa32@utexas.edu>
2021-04-02 19:34:55 -0500

    Alex working on connection stuff

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const Web3 = require('web3');
+const web3 = new Web3('http://localhost:8545');
+const bytecode = fs.readFileSync('./build/ballot_sol_Poll.bin');
+const abi = JSON.parse(fs.readFileSync('./build/ballot_sol_Poll.abi'));
+
+(async function () {
+  const ganacheAccounts = await web3.eth.getAccounts();
+  const myWalletAddress = ganacheAccounts[0];
+
+  const myContract = new web3.eth.Contract(abi);
+
+  myContract.deploy({
+    data: bytecode.toString(),
+    arguments: ["Omg is this working?","0xC7854Ff4CB030f6B0aC373abFF8937EeD08b83bC"]
+  }).send({
+    from: myWalletAddress,
+    gas: 5000000
+  }).then((deployment) => {
+    console.log('FirstContract was successfully deployed!');
+    console.log('FirstContract can be interfaced with at this address:');
+    console.log(deployment.options.address);
+  }).catch((err) => {
+    console.error(err);
+  });
+})();
