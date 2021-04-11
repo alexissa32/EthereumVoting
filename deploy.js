63abb251816589042efe19b0815a6e2b7b9b21ad
Alex Issa <alex.issa32@utexas.edu>
2021-04-11 01:30:08 -0500

    works on remix but not locally with ganache and after hours of debugging my version and Yue's version on her branch, we are so confused...

--- deploy.js
+++ deploy.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
-const bytecode = fs.readFileSync('./build/ballot_sol_Poll.bin');
-const abi = JSON.parse(fs.readFileSync('./build/ballot_sol_Poll.abi'));
+const bytecode = fs.readFileSync('./_registry.bin');
+const abi = JSON.parse(fs.readFileSync('./_registry.abi'));
 
 (async function () {
   const ganacheAccounts = await web3.eth.getAccounts();
@@ -12,7 +12,7 @@ const abi = JSON.parse(fs.readFileSync('./build/ballot_sol_Poll.abi'));
 
   myContract.deploy({
     data: bytecode.toString(),
-    arguments: ["Omg is this working?","0xC7854Ff4CB030f6B0aC373abFF8937EeD08b83bC"]
+    //arguments: ["Omg is this working?","0xC7854Ff4CB030f6B0aC373abFF8937EeD08b83bC"]
   }).send({
     from: myWalletAddress,
     gas: 5000000
---
+++ get-past-logs.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+const Web3 = require("web3");
+// Dai Stablecoin ABI
+const abi = JSON.parse(fs.readFileSync('./_registry.abi'));
+const INFURA_URL =
+    "http://localhost:8545";
+
+const web3 = new Web3(INFURA_URL);
+
+// Address of Contract
+const address = "0x5BB5f390E5F646Dab25cB8996728252F6FB37abd";
+
+const contract = new web3.eth.Contract(abi, address);
+
+
+
+async function make_transaction() {
+
+    // my wallet info
+    const ganacheAccounts = await web3.eth.getAccounts();
+    const myWalletAddress = ganacheAccounts[0];
+    // suppose you want to call a function named myFunction of myContract
+    contract.methods.createPoll("Is poll contract created?").send(
+        {
+
+            from: myWalletAddress,
+        }
+    )
+    .on('transactionHash', function(hash){
+        console.log("1")
+    })
+    .on('receipt', function(receipt){
+        console.log("2")
+    })
+    .on('confirmation', function(confirmationNumber, receipt){
+        console.log("3")
+    })
+    .on('error', function(error, receipt) {
+        console.log("4")
+    });  
+}
+make_transaction();
+
+async function make_transaction_registry() {
+
+    // my wallet info
+    const ganacheAccounts = await web3.eth.getAccounts();
+    const myWalletAddress = ganacheAccounts[2];
+    // suppose you want to call a function named myFunction of myContract
+    await contract.methods.createNewRecord("0xdF0cc4221d7B2f716cc55e511A388BC74855A599", 2).send(
+        {
+
+            from: myWalletAddress,
+        }
+    )
+
+    console.log("registry transaction successfullly made!")
+}
+// make_transaction_registry();
+
+
+
+
+
+async function main() {
+    // web3.eth_sendTransaction()
+    const latest = await web3.eth.getBlockNumber();
+
+    console.log("Latest block:", latest);
+
+
+
+    const logs = await contract.getPastEvents("Record", {
+        fromBlock: latest - 100,
+        toBlock: latest,
+        // filter by sender
+        // filter: { sender: "0xF4F0F367Fe3B142fc9E643F3E328377f8F0a8301" }
+        filter: {
+            role: 2,
+            sender: "0xA5848ec73478813B2AB65e927D55aA30B3EAf25A"
+        }
+
+        //filter by receiver
+        // filter: { address: "0x56D3CF60317B5ad5d868323350b5DfaE20f90661" }
+    });
+
+    console.log("Logs", logs, `${logs.length} logs`);
+
+    // Print senders
+    console.log(
+        "Polls",
+        logs.map(log => log.returnValues.poll),
+        `${logs.length} logs`
+    );
+
+}
+
+// main();
